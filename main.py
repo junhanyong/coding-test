daa323e021d20e388e41906d2d423bfda618ce6e
junhanyong <jhyong08@gmail.com>
2025-10-02 11:13:52 +0900

    Added read functions

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine, Integer, String, Boolean, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 # Create connection to SQLite DB
 engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False})
@@ -45,12 +46,24 @@ class TodoResponse(BaseModel):
     class Config:
         orm_mode = True
 
-# id, completed automatically generated
 @app.post("/todo/", response_model=TodoResponse) 
-def create(todo: TodoCreate, db: Session = Depends(get_db)):
+def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
     db_todo = Todo(title=todo.title)
     db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
     return db_todo
+
+@app.get("/todo/{todo_id}", response_model=TodoResponse)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(todo_id == Todo.id).first()
+    # Check if id exists
+    if not todo:
+        raise HTTPException(status_code=404, detail="ID does not exist")
+    return todo
+
+@app.get("/todo/", response_model=List[TodoResponse])
+def read_todos(db: Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+    return todos
 
